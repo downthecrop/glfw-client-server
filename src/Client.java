833593ef878a39c912b7c13b14096a047c2b7342
downthecrop <bryce@mathx.ca>
2023-10-22 02:43:23 -0700

    Add wrappers

--- src/Client.java
+++ src/Client.java
@@ -1,5 +1,10 @@
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.FloatBuffer;
 
 public class Client {
 
@@ -18,6 +23,231 @@ public class Client {
     public static void glClearColor(float red, float green, float blue, float alpha) {
         Object[] args = new Object[]{red, green, blue, alpha};
         sendCommand("glClearColor", args);
+    }
+
+    public static void glEnable(int cap) {
+        sendCommand("glEnable", cap);
+    }
+
+    public static void glDisable(int cap) {
+        sendCommand("glDisable", cap);
+    }
+
+    public static void glViewport(int x, int y, int width, int height) {
+        Object[] args = new Object[]{x, y, width, height};
+        sendCommand("glViewport", args);
+    }
+
+    public static void glClear(int mask) {
+        Object[] args = new Object[]{mask};
+        sendCommand("glClear", args);
+    }
+
+    public static void glLoadMatrixf(FloatBuffer matrix) {
+        float[] array = new float[matrix.remaining()];
+        matrix.get(array);
+        Object[] args = new Object[]{array};
+        sendCommand("glLoadMatrixf", args);
+    }
+
+    public static void glLineWidth(float width) {
+        Object[] args = new Object[]{width};
+        sendCommand("glLineWidth", args);
+    }
+
+    public static void glRotatef(float angle, float x, float y, float z) {
+        Object[] args = new Object[]{angle, x, y, z};
+        sendCommand("glRotatef", args);
+    }
+
+    public static void glMatrixMode(int mode) {
+        sendCommand("glMatrixMode", mode);
+    }
+
+
+    public static void glReadBuffer(int mode) {
+        sendCommand("glReadBuffer", mode);
+    }
+
+
+    public static void glDrawBuffer(int mode) {
+        sendCommand("glDrawBuffer", mode);
+    }
+
+    public static void glTexEnvi(int target, int pname, int param) {
+        Object[] args = new Object[]{target, pname, param};
+        sendCommand("glTexEnvi", args);
+    }
+
+    public static void glBindTexture(int target, int texture) {
+        Object[] args = new Object[]{target, texture};
+        sendCommand("glBindTexture", args);
+    }
+
+    public static void glShadeModel(int mode) {
+        Object[] args = new Object[]{mode};
+        sendCommand("glShadeModel", args);
+    }
+
+    public static void glClearDepth(double depth) {
+        Object[] args = new Object[]{depth};
+        sendCommand("glClearDepth", args);
+    }
+
+    public static void glDepthFunc(int func) {
+        Object[] args = new Object[]{func};
+        sendCommand("glDepthFunc", args);
+    }
+
+
+    public static void glDepthMask(boolean flag) {
+        Object[] args = new Object[]{flag};
+        sendCommand("glDepthMask", args);
+    }
+
+    public static void glLoadIdentity() {
+        Object[] args = new Object[]{};
+        sendCommand("glLoadIdentity", args);
+    }
+
+    public static void glPolygonMode(int face, int mode) {
+        Object[] args = new Object[]{face, mode};
+        sendCommand("glPolygonMode", args);
+    }
+
+    public static void glCullFace(int mode) {
+        Object[] args = new Object[]{mode};
+        sendCommand("glCullFace", args);
+    }
+
+
+    public static void glBlendFunc(int sfactor, int dfactor) {
+        Object[] args = new Object[]{sfactor, dfactor};
+        sendCommand("glBlendFunc", args);
+    }
+
+    public static void glAlphaFunc(int func, float ref) {
+        Object[] args = new Object[]{func, ref};
+        sendCommand("glAlphaFunc", args);
+    }
+
+    public static void glEnableClientState(int cap) {
+        Object[] args = new Object[]{cap};
+        sendCommand("glEnableClientState", args);
+    }
+
+    public static int glGetInteger(int pname) {
+        Object[] args = new Object[]{pname};
+        return (int) sendCommandWithReturn("glGetInteger", args);
+    }
+
+
+    public static int[] glGenTextures(int n) {
+        int[] textureIDs = null;
+        Object[] args = new Object[]{n};
+        try {
+            textureIDs = (int[]) sendCommandWithReturn("glGenTextures", args);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return textureIDs;
+    }
+
+
+
+    public static void glPushAttrib(int mask) {
+        Object[] args = new Object[]{mask};
+        sendCommand("glPushAttrib", args);
+    }
+
+    public static void glRasterPos2i(int x, int y) {
+        Object[] args = new Object[]{x, y};
+        sendCommand("glRasterPos2i", args);
+    }
+
+    public static void glCopyPixels(int x, int y, int width, int height, int type) {
+        Object[] args = new Object[]{x, y, width, height, type};
+        sendCommand("glCopyPixels", args);
+    }
+
+    public static void glPopAttrib() {
+        sendCommand("glPopAttrib", null);
+    }
+
+    public static void glOrtho(double left, double right, double bottom, double top, double near, double far) {
+        Object[] args = new Object[]{left, right, bottom, top, near, far};
+        sendCommand("glOrtho", args);
+    }
+
+    public static void glTranslatef(float x, float y, float z) {
+        Object[] args = new Object[]{x, y, z};
+        sendCommand("glTranslatef", args);
+    }
+
+    public static void glDisableClientState(int cap) {
+        Object[] args = new Object[]{cap};
+        sendCommand("glDisableClientState", args);
+    }
+
+    public static void glDeleteTextures(int[] textures) {
+        Object[] args = new Object[]{textures};
+        sendCommand("glDeleteTextures", args);
+    }
+
+    public static void glTexImage3D(int target, int level, int internalFormat, int width, int height, int depth, int border, int format, int type, ByteBuffer pixels) {
+        Object[] args = new Object[]{target, level, internalFormat, width, height, depth, border, format, type, pixels};
+        sendCommand("glTexImage3D", args);
+    }
+
+
+    public static void glTexParameteri(int target, int pname, int param) {
+        Object[] args = new Object[]{target, pname, param};
+        sendCommand("glTexParameteri", args);
+    }
+
+    public static void glTexImage2D(int target, int level, int internalformat, int width, int height, int border, int format, int type, ByteBuffer pixels) {
+        try {
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
+
+            objectOutputStream.writeObject(pixels.array());
+
+            byte[] pixelBytes = byteArrayOutputStream.toByteArray();
+
+            Object[] args = new Object[]{target, level, internalformat, width, height, border, format, type, pixelBytes};
+            sendCommand("glTexImage2D", args);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static Object sendCommandWithReturn(String command, Object args) {
+        try (Socket socket = new Socket("localhost", 8080)) {
+            ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+            ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+
+            out.writeUTF(command);
+            out.writeObject(args);
+            out.flush();
+
+            return in.readObject();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void glActiveTexture(int texture) {
+        Object[] args = new Object[]{texture};
+        sendCommand("glActiveTexture", args);
+    }
+
+    public static void glDrawElementsWrapper(int mode, int count, int type, ByteBuffer buffer) {
+        byte[] byteArray = new byte[buffer.remaining()];
+        buffer.get(byteArray);
+
+        Object[] args = new Object[] { mode, count, type, byteArray };
+        sendCommand("glDrawElementsWrapper", args);
     }
 
     private static void sendCommand(String command, Object args) {
@@ -42,6 +272,7 @@ public class Client {
         try {
             while (true) {
                 glClearColor(1, 0, 0, 1);
+                glLineWidth(1000f);
                 Thread.sleep(1000);
                 glClearColor(0, 0, 1, 1);
                 Thread.sleep(1000);
--- src/Server.java
+++ src/Server.java
@@ -1,22 +1,43 @@
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
+import java.nio.FloatBuffer;
 
 import org.lwjgl.glfw.GLFW;
 import org.lwjgl.opengl.GL;
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL12;
 import org.lwjgl.opengl.GL20;
+import org.lwjgl.system.MemoryUtil;
 
 public class Server {
     public static void main(String[] args) {
+        int level;
+        int target;
+        int texture;
+        int pname;
+        int width;
+        int border;
+        int height;
+        int x;
+        int y;
+        int cap;
+        int type;
+        int mode;
+        int mask;
+        int format;
+        int param;
         try (ServerSocket serverSocket = new ServerSocket(8080)) {
 
             if (!GLFW.glfwInit()) {
                 System.err.println("Failed to initialize GLFW!");
                 System.exit(1);
             }
-
+            
             long window = GLFW.glfwCreateWindow(800, 600, "Basic GLFW Window", 0, 0);
             if (window == 0) {
                 System.err.println("Failed to create a GLFW window!");
@@ -25,20 +46,280 @@ public class Server {
             }
 
             GLFW.glfwMakeContextCurrent(window);
-            GL.createCapabilities();
 
             while (true) {
+                GL.createCapabilities();
                 try (Socket socket = serverSocket.accept()) {
                     ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+                    ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
 
                     while (true) {
                         String command = in.readUTF();
                         Object argsObject = in.readObject();
-
+                        if(!command.equals(""))
+                            System.out.println("Got a command: "+command);
                         switch (command) {
                             case "glClear":
-                                int mask = (int) argsObject;
+                                mask = (int) argsObject;
                                 GL20.glClear(mask);
+                                break;
+
+                            case "glGenTextures":
+                                int[] textureIds = new int[1];
+                                GL11.glGenTextures(textureIds);
+                                out.writeObject(textureIds);
+                                out.flush();
+                                break;
+
+                            case "glEnable":
+                                cap = (int) argsObject;
+                                GL20.glEnable(cap);
+                                break;
+
+                            case "glDisable":
+                                int capDisable = (int) argsObject;
+                                GL20.glDisable(capDisable);
+                                break;
+
+                            case "glMatrixMode":
+                                mode = (int) argsObject;
+                                GL11.glMatrixMode(mode);
+                                break;
+
+                            case "glDepthMask":
+                                boolean flag = (boolean) argsObject;
+                                GL11.glDepthMask(flag);
+                                break;
+
+                            case "glLoadIdentity":
+                                GL11.glLoadIdentity();
+                                break;
+
+                            case "glPolygonMode":
+                                Object[] polyArgs = (Object[]) argsObject;
+                                int face = (int) polyArgs[0];
+                                mode = (int) polyArgs[1];
+                                GL11.glPolygonMode(face, mode);
+                                break;
+
+                            case "glCullFace":
+                                mode = (int) argsObject;
+                                GL11.glCullFace(mode);
+                                break;
+
+                            case "glBlendFunc":
+                                Object[] blendArgs = (Object[]) argsObject;
+                                int sfactor = (int) blendArgs[0];
+                                int dfactor = (int) blendArgs[1];
+                                GL11.glBlendFunc(sfactor, dfactor);
+                                break;
+
+                            case "glEnableClientState":
+                                Object[] enableClientStateArgs = (Object[]) argsObject;
+                                cap = (int) enableClientStateArgs[0];
+                                GL11.glEnableClientState(cap);
+                                break;
+
+                            case "glGetInteger":
+                                pname = (int)((Object[]) argsObject)[0];
+                                int result = GL11.glGetInteger(pname);
+                                out.writeObject(result);
+                                break;
+
+                            case "glAlphaFunc":
+                                Object[] alphaArgs = (Object[]) argsObject;
+                                int func = (int) alphaArgs[0];
+                                float ref = (float) alphaArgs[1];
+                                GL11.glAlphaFunc(func, ref);
+                                break;
+
+                            case "glDrawBuffer":
+                                mode = (int) argsObject;
+                                GL11.glDrawBuffer(mode);
+                                break;
+
+                            case "glActiveTexture":
+                                texture = (int) argsObject;
+                                GL20.glActiveTexture(texture);
+                                break;
+
+                            case "glBindTexture":
+                                Object[] bindTextureArgs = (Object[]) argsObject;
+                                target = (int) bindTextureArgs[0];
+                                texture = (int) bindTextureArgs[1];
+                                GL11.glBindTexture(target, texture);
+                                break;
+
+                            case "glTexEnvi":
+                                Object[] texEnviArgs = (Object[]) argsObject;
+                                target = (int) texEnviArgs[0];
+                                pname = (int) texEnviArgs[1];
+                                param = (int) texEnviArgs[2];
+                                GL11.glTexEnvi(target, pname, param);
+                                break;
+
+                            case "glReadBuffer":
+                                mode = (int) argsObject;
+                                GL11.glReadBuffer(mode);
+                                break;
+
+                            case "glPushAttrib":
+                                mask = (int)((Object[]) argsObject)[0];
+                                GL11.glPushAttrib(mask);
+                                break;
+
+                            case "glRasterPos2i":
+                                Object[] rasterPosArgs = (Object[]) argsObject;
+                                x = (int) rasterPosArgs[0];
+                                y = (int) rasterPosArgs[1];
+                                GL11.glRasterPos2i(x, y);
+                                break;
+
+                            case "glCopyPixels":
+                                Object[] copyPixelsArgs = (Object[]) argsObject;
+                                x = (int) copyPixelsArgs[0];
+                                y = (int) copyPixelsArgs[1];
+                                width = (int) copyPixelsArgs[2];
+                                height = (int) copyPixelsArgs[3];
+                                type = (int) copyPixelsArgs[4];
+                                GL11.glCopyPixels(x, y, width, height, type);
+                                break;
+
+                            case "glPopAttrib":
+                                GL11.glPopAttrib();
+                                break;
+
+                            case "glOrtho":
+                                Object[] orthoArgs = (Object[]) argsObject;
+                                double left = (double) orthoArgs[0];
+                                double right = (double) orthoArgs[1];
+                                double bottom = (double) orthoArgs[2];
+                                double top = (double) orthoArgs[3];
+                                double near = (double) orthoArgs[4];
+                                double far = (double) orthoArgs[5];
+                                GL11.glOrtho(left, right, bottom, top, near, far);
+                                break;
+
+                            case "glTranslatef":
+                                Object[] translateArgs = (Object[]) argsObject;
+                                float x2 = (float) translateArgs[0];
+                                float y2 = (float) translateArgs[1];
+                                float z2 = (float) translateArgs[2];
+                                GL11.glTranslatef(x2, y2, z2);
+                                break;
+
+                            case "glDisableClientState":
+                                Object[] clientStateArgs = (Object[]) argsObject;
+                                cap = (int) clientStateArgs[0];
+                                GL11.glDisableClientState(cap);
+                                break;
+
+                            case "glDeleteTextures":
+                                Object[] textureArgs = (Object[]) argsObject;
+                                int[] textures = (int[]) textureArgs[0];
+                                GL11.glDeleteTextures(textures);
+                                break;
+
+                            case "glTexImage3D":
+                                Object[] texImage3DArgs = (Object[]) argsObject;
+                                target = (int) texImage3DArgs[0];
+                                level = (int) texImage3DArgs[1];
+                                int internalFormat = (int) texImage3DArgs[2];
+                                width = (int) texImage3DArgs[3];
+                                height = (int) texImage3DArgs[4];
+                                int depth = (int) texImage3DArgs[5];
+                                border = (int) texImage3DArgs[6];
+                                format = (int) texImage3DArgs[7];
+                                type = (int) texImage3DArgs[8];
+                                ByteBuffer pixels = (ByteBuffer) texImage3DArgs[9];
+
+                                GL12.glTexImage3D(target, level, internalFormat, width, height, depth, border, format, type, pixels);
+                                break;
+
+                            case "glTexParameteri":
+                                Object[] texParamArgs = (Object[]) argsObject;
+                                target = (int) texParamArgs[0];
+                                pname = (int) texParamArgs[1];
+                                param = (int) texParamArgs[2];
+
+                                GL11.glTexParameteri(target, pname, param);
+                                break;
+
+                            case "glTexImage2D":
+                                Object[] texImage2DArgs = (Object[]) argsObject;
+
+                                target = (int) texImage2DArgs[0];
+                                level = (int) texImage2DArgs[1];
+                                int internalformat = (int) texImage2DArgs[2];
+                                width = (int) texImage2DArgs[3];
+                                height = (int) texImage2DArgs[4];
+                                border = (int) texImage2DArgs[5];
+                                format = (int) texImage2DArgs[6];
+                                type = (int) texImage2DArgs[7];
+                                byte[] pixelBytes = (byte[]) texImage2DArgs[8];
+
+                                ByteBuffer pixels1 = ByteBuffer.wrap(pixelBytes);
+
+                                GL11.glTexImage2D(target, level, internalformat, width, height, border, format, type, pixels1);
+                                break;
+
+                            case "glDrawElementsWrapper":
+                                Object[] drawElementsArgs = (Object[]) argsObject;
+                                mode = (int) drawElementsArgs[0];
+                                int count = (int) drawElementsArgs[1];
+                                type = (int) drawElementsArgs[2];
+                                byte[] byteArray = (byte[]) drawElementsArgs[3];
+
+                                ByteBuffer buffer = MemoryUtil.memAlloc(byteArray.length).put(byteArray).flip();
+                                long pointer = MemoryUtil.memAddress(buffer);
+
+                                GL11.glDrawElements(mode, count, type, pointer);
+
+                                MemoryUtil.memFree(buffer);
+                                break;
+
+                            case "glClearDepth":
+                                GL11.glClearDepth((double) argsObject);
+                                break;
+
+                            case "glLoadMatrixf":
+                                Object[] matrixArgs = (Object[]) argsObject;
+                                float[] array = (float[]) matrixArgs[0];
+
+                                FloatBuffer buffer2 = ByteBuffer.allocateDirect(array.length * 4).asFloatBuffer();
+                                buffer2.put(array);
+                                buffer2.flip();
+
+                                GL11.glLoadMatrixf(buffer2);
+                                break;
+
+                            case "glRotatef":
+                                Object[] rotateArgs = (Object[]) argsObject;
+                                float angle = (float) rotateArgs[0];
+                                float x3 = (float) rotateArgs[1];
+                                float y3 = (float) rotateArgs[2];
+                                float z3 = (float) rotateArgs[3];
+
+                                GL11.glRotatef(angle, x3, y3, z3);
+                                break;
+
+                            case "glLineWidth":
+                                float width3 = (float) ((Object[]) argsObject)[0];
+                                GL11.glLineWidth(width3);
+                                break;
+
+                            case "glViewport":
+                                Object[] viewportArgs = (Object[]) argsObject;
+                                x = (int) viewportArgs[0];
+                                y = (int) viewportArgs[1];
+                                width = (int) viewportArgs[2];
+                                height = (int) viewportArgs[3];
+
+                                GL11.glViewport(x, y, width, height);
+                                break;
+
+                            case "glDepthFunc":
+                                GL11.glDepthFunc((int) argsObject);
                                 break;
 
                             case "glClearColor":
@@ -50,10 +331,18 @@ public class Server {
                                 GL20.glClearColor(red, green, blue, alpha);
                                 break;
 
+                            case "glShadeModel":
+                                mode = (int) argsObject;
+                                GL11.glShadeModel(mode);
+                                break;
+
                             default:
                                 System.out.println("Unrecognized command: " + command);
                         }
 
+                        if(GL20.glGetError() != 0){
+                            System.out.println("Error");
+                        }
                         GLFW.glfwPollEvents();
                         GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
                         GLFW.glfwSwapBuffers(window);
